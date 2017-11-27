32d558d3038dea8ccaffa65edccdc15715969c9c
zahmed2003 <league@wts10.attlocal.net>
2017-11-26 17:25:02 -0800

    warm up, animal farm, ice cream

---
+++ src/warmUp.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class warmUp {
+public static void main(String[] args) {
+ String lolipop = JOptionPane.showInputDialog("What is the word?");
+for (int i = 0; i <5; i++) {
+	
+System.out.println(lolipop);
+	
+}
+
+
+
+}
+
+
+
+
+
+}
